d0dfa1907ba1aa8be67f0e4e0e01c585f8706d4b
Elmo <Shawnsamudra09@users.noreply.github.com>
2020-04-20 16:19:46 +1000

    asdf

--- mylibraryapp/models/index.js
+++ mylibraryapp/models/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
 // Connect to MongoDB --- Replace this with your Connection String
-CONNECTION_STRING = "mongodb+srv://Shawn:LXgK844kPPSNFtJm@cluster0-m0kei.mongodb.net/test?retryWrites=true&w=majority";
+CONNECTION_STRING = "mongodb+srv://Shawn:<password>@cluster0-m0kei.mongodb.net/test?retryWrites=true&w=majority";
 MONGO_URL =
 CONNECTION_STRING.replace("<password>",process.env.MONGO_PASSWORD);
 mongoose.connect(MONGO_URL || "mongodb://localhost/info30005", {
